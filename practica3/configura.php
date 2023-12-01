43f751fa75da3be085bf116352580677d21b3c58
ahernandezm1001 <ahernandezm1001@gmail.com>
2023-11-30 20:58:34 -0600

    Actualización comentario

--- practica3/configura.php
+++ practica3/configura.php
@@ -20,7 +20,7 @@ echo"<table>
             </tr>
         </thead>
         <tr>
-        
+        //webooooooos
             <td colspan='2'>Periodo</td>
             <td>Direccion</td>
             <td>Reconocimiento</td>
