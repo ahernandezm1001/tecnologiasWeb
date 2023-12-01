968bd8e3ae0b62049819355def185d712b3271f1
ahernandezm1001 <ahernandezm1001@gmail.com>
2023-11-30 20:31:25 -0600

    mi push

---
+++ practica3/configura.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="../estilos/tablas.css">
+    <title>Configuracion</title>
+</head>
+<body>
+<?php
+echo"<table>
+        <thead>
+            <tr>
+            <th colspan='5'>Primaria: Emiliano Zapata</th>
+            <!-- Celda expande a 5 
+            <td></td>
+            <td></td>
+            <td></td>
+            <td></td>-->
+            </tr>
+        </thead>
+        <tr>
+            <td colspan='2'>Periodo</td>
+            <td>Direccion</td>
+            <td>Reconocimiento</td>
+            <td>Promedio</td>
+            
+        </tr>
+        <tr>
+            <td>2012</td>
+            <td>2015</td>
+            <td>direcion xd</td>
+            <td>diploma de finalizacion</td>
+            <td>10</td>
+        </tr>
+    </table>";
+    echo "<h1>";
+    echo "hola mundo";
+    echo "</h1>";
+    
+?>  
+
+</body>
